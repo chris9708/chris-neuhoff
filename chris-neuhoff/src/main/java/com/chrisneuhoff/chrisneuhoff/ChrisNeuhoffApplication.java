f482d108e55674b1a4c697c5108fcf476022f2c5
chris <chrisneuhoff1@gmail.com>
2023-04-28 00:08:54 +0200

    init commit

---
+++ chris-neuhoff/src/main/java/com/chrisneuhoff/chrisneuhoff/ChrisNeuhoffApplication.java
@@ -0,0 +1,13 @@
+package com.chrisneuhoff.chrisneuhoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrisNeuhoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrisNeuhoffApplication.class, args);
+	}
+
+}
---
+++ chris-neuhoff/src/test/java/com/chrisneuhoff/chrisneuhoff/ChrisNeuhoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisneuhoff.chrisneuhoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrisNeuhoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
